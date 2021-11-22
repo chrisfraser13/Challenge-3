8021ca62a82df7cd3148d8a04a0a5ac09073e6b3
Chris Fraser <chrisfraser@Chriss-MBP.home>
2021-11-21 20:51:04 -0500

    final commit

--- script.js
+++ script.js
@@ -27,9 +27,37 @@ function generatePassword() {
     alert("You must input a number!");
     return generatePassword
   }
+  if(length<8 || length>128){
+    alert("How long would you like your password to be? (between 8 - 128)");
+    return generatePassword()
+  }
+
+  var hasUpper = confirm("Include upper case letters?");
+  var hasLower = confirm("Include lower case letters?");
+  var hasNumbers = confirm("Include numbers?");
+  var hasSpecial = confirm("Include special characters?");
+
+  if(!hasUpper&&hasLower&!hasNumbers&&!hasSpecial){
+    alert("You have to choose atleast 1 character type!");
+    return generatePassword()
+  }
+  if(hasUpper){
+    chosenCharacters += upper
+  }
+  if(hasLower){
+    chosenCharacters += lower
+  }
+  if(hasNumbers){
+    chosenCharacters += numbers
+  }
+  if(hasSpecial){
+    chosenCharacters += special
+  }
+for (var i = 0; i < length; i++) {
+  result += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharacters.length));
 }
-
-
+return result;
+}
 
 
 // Write password to the #password input
